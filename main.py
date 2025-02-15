268e64c5f26aeba91751c598050d33f9288d98cb
Liuperson306 <129485929+Liuperson306@users.noreply.github.com>
2025-02-15 21:33:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -279,7 +279,7 @@ def page(video_num, method_num):
 
 
 if __name__ == '__main__':
-    st.set_page_config(page_title="userstudy")
+    st.set_page_config(page_title="userstudy", layout="wide")
     #st.cache_data.clear() # 初始化
     myemail = st.secrets["my_email"]["email"]  
     password = st.secrets["my_email"]["password"]
